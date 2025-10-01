9ac307d2b8a654dc9bc9e1276aa3723d7b579765
PShivakumar <80328475+PixelShiv@users.noreply.github.com>
2025-10-01 21:29:03 +0530

    Update deployToTomcat.groovy

--- vars/deployToTomcat.groovy
+++ vars/deployToTomcat.groovy
@@ -1,6 +1,26 @@
-def call(String warName = "target/*.war", String tomcatPath = "/opt/tomcat/webapps") {
-    echo "Deploying WAR to Tomcat..."
-    sh "sudo cp ${warName} ${tomcatPath}/"
-    echo "Deployment complete!"
+def call(String warName = "target/*.war", 
+         String tomcatUser = "ec2-user", 
+         String tomcatHost = "your.ec2.ip.addr", 
+         String tomcatPath = "/opt/tomcat/webapps/", 
+         String appName = "app") {
+
+    echo "Deploying WAR to Tomcat on remote host ${tomcatHost}..."
+
+    // Copy WAR to remote host
+    sh """
+        scp -o StrictHostKeyChecking=no ${warName} ${tomcatUser}@${tomcatHost}:/home/${tomcatUser}/${appName}.war
+    """
+
+    // Deploy on remote host
+    sh """
+        ssh -o StrictHostKeyChecking=no ${tomcatUser}@${tomcatHost} "
+            sudo mv /home/${tomcatUser}/${appName}.war ${tomcatPath}${appName}.war && \
+            if [ -d ${tomcatPath}${appName} ]; then sudo rm -rf ${tomcatPath}${appName}; fi && \
+            sudo chown -R tomcat:tomcat ${tomcatPath}${appName}.war && \
+            sudo systemctl restart tomcat || true
+        "
+    """
+
+    echo "Deployment to Tomcat complete!"
 }
 
