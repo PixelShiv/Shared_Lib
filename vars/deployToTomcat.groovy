fac88b92d0dbb93c06c4701005673accf4baee24
PShivakumar <80328475+PixelShiv@users.noreply.github.com>
2025-10-01 20:29:32 +0530

    Update deployToTomcat.groovy

--- vars/deployToTomcat.groovy
+++ vars/deployToTomcat.groovy
@@ -1,1 +1,6 @@
+def call(String warName = "target/*.war", String tomcatPath = "/opt/tomcat/webapps") {
+    echo "Deploying WAR to Tomcat..."
+    sh "cp ${warName} ${tomcatPath}/"
+    echo "Deployment complete!"
+}
 
