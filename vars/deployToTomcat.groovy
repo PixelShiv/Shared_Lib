732dce9aa0bece14755f7da38e6f8df2490a4da8
PShivakumar <80328475+PixelShiv@users.noreply.github.com>
2025-10-01 21:55:07 +0530

    Update deployToTomcat.groovy

--- vars/deployToTomcat.groovy
+++ vars/deployToTomcat.groovy
@@ -2,25 +2,27 @@ def call(String warName = "target/*.war",
          String tomcatUser = "ec2-user", 
          String tomcatHost = "34.207.147.216", 
          String tomcatPath = "/opt/tomcat/webapps/", 
-         String appName = "app") {
+         String appName = "app",
+         String sshCredsId = "ec2-key") {  // Jenkins credential ID for the .pem key
 
     echo "Deploying WAR to Tomcat on remote host ${tomcatHost}..."
 
-    // Copy WAR to remote host
-    sh """
-        scp -o StrictHostKeyChecking=no ${warName} ${tomcatUser}@${tomcatHost}:/home/${tomcatUser}/${appName}.war
-    """
+    sshagent([sshCredsId]) {
+        // Copy WAR to remote host
+        sh """
+            scp -o StrictHostKeyChecking=no ${warName} ${tomcatUser}@${tomcatHost}:/home/${tomcatUser}/${appName}.war
+        """
 
-    // Deploy on remote host
-    sh """
-        ssh -o StrictHostKeyChecking=no ${tomcatUser}@${tomcatHost} "
-            sudo mv /home/${tomcatUser}/${appName}.war ${tomcatPath}${appName}.war && \
-            if [ -d ${tomcatPath}${appName} ]; then sudo rm -rf ${tomcatPath}${appName}; fi && \
-            sudo chown -R tomcat:tomcat ${tomcatPath}${appName}.war && \
-            sudo systemctl restart tomcat || true
-        "
-    """
+        // Deploy on remote host
+        sh """
+            ssh -o StrictHostKeyChecking=no ${tomcatUser}@${tomcatHost} "
+                mv /home/${tomcatUser}/${appName}.war ${tomcatPath}${appName}.war && \
+                if [ -d ${tomcatPath}${appName} ]; then rm -rf ${tomcatPath}${appName}; fi && \
+                chown -R ${tomcatUser}:${tomcatUser} ${tomcatPath}${appName}.war && \
+                systemctl restart tomcat || true
+            "
+        """
+    }
 
     echo "Deployment to Tomcat complete!"
 }
-
